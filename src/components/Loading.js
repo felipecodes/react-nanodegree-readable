2c46aa93c97e2e5ee6b35036b762c2640f55dc19
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-19 20:57:39 -0200

    add: linear progress component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import { LinearProgress } from 'material-ui'
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`
 
 const Loading = () => (
-  <div>Loading...</div>
+  <Wrapper>
+    <LinearProgress />
+  </Wrapper>
 )
 
 export default Loading
