84edef339e2b90287777ffec4fc27c0bf32b08ae
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-19 18:44:46 -0200

    fix: short-circuit of category list

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const CategoryList = props => (
   <div>
     <h1>CategoryList</h1>
-    {props.categories.length && (
+    {!!props.categories.length && (
       <ul>
         {props.categories.map(category => (
           <li key={category.path}>
