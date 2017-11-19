c14c111fc4fbcfaffcc4e1ea2165bf75ea0a3b85
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-19 13:39:04 -0200

    add: styled-components and styled-normalize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { injectGlobal } from 'styled-components'
 import { BrowserRouter as Router } from 'react-router-dom'
+import styledNormalize from 'styled-normalize'
 import App from './containers/App'
 import configureStore from './store/configureStore'
+
+injectGlobal`${styledNormalize}`
 
 const store = configureStore()
 
