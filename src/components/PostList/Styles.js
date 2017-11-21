7a97901c355ec30d25d312608acc97039a5686fe
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-21 00:49:21 -0200

    add: styles of post vote score

--- src/components/PostList/Styles.js
+++ src/components/PostList/Styles.js
@@ -5,12 +5,44 @@ const Wrapper = styled.div`
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.25);
 `
 
+const List = styled.ul`
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-left: 0;
+  list-style: none;
+`
+
+const Item = styled.li`
+  display: flex;
+  border-bottom: 1px solid #eee;
+`
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 8px;
+  padding-left: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eee;
+  }
+`
+
 const CustomLink = styled(Link)`
   display: inline-block;
   padding: 16px;
   width: 100%;
   text-decoration: none;
-  color: #000000de;
+  color: #000000;
 `
 
-export { Wrapper, CustomLink }
+const Score = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 6px;
+  font-size: 14px;
+  color: #555;
+`
+
+export { Wrapper, List, Item, Score, IconWrapper, CustomLink }
--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import { List, ListItem } from 'material-ui'
+import ThumbDown from 'material-ui/svg-icons/action/thumb-down'
+import ThumbUp from 'material-ui/svg-icons/action/thumb-up'
 import Wrap from '../Wrap'
 import BoxTitle from '../common/BoxTitle'
-import { Wrapper, CustomLink } from './Styles'
+import { Wrapper, List, Item, Score, IconWrapper, CustomLink } from './Styles'
 
 const PostList = props => (
   <Wrapper>
     <List>
       {props.posts.map(post => (
-        <ListItem innerDivStyle={{padding: 0}} key={post.id}>
+        <Item
+          key={post.id}
+          innerDivStyle={{
+            display: 'flex',
+            padding: 0
+          }}
+        >
           <CustomLink to={`/posts/${post.id}`}>{post.title}</CustomLink>
-        </ListItem>
+          <Score>{post.voteScore}</Score>
+          <IconWrapper onClick={props.vote}>
+            <ThumbUp
+              style={{
+                width: '16px',
+                color: '#08b135'
+              }}
+            />
+          </IconWrapper>
+          <IconWrapper onClick={props.vote}>
+            <ThumbDown
+              style={{
+                width: '16px',
+                color: '#e02100'
+              }}
+            />
+          </IconWrapper>
+        </Item>
       ))}
     </List>
   </Wrapper>
