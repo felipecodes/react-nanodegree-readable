12ef8e1c0b6419ab01ab9713bd1e0ceb5d1e00fa
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-25 21:28:18 -0200

    fix: date format of post detail

--- src/reducers/postDetails.js
+++ src/reducers/postDetails.js
@@ -1,11 +1,15 @@
 import { RECEIVE_POST } from '../actions'
+import { getFormatedDate } from '../util/dateUtils'
 
 function postDetails(state = {}, action) {
   switch (action.type) {
     case RECEIVE_POST:
       return {
         ...state,
-        [action.post.id]: action.post
+        [action.post.id]: {
+          ...action.post,
+          date: getFormatedDate(action.post)
+        }
       }
 
     default:
