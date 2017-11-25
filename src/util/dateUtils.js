5ce7c26f6959709ea60a883c30969a5a010d686e
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-25 21:26:19 -0200

    add: date utils

---
+++ src/util/dateUtils.js
@@ -0,0 +1,6 @@
+const getFormatedDate = ({ timestamp }) => {
+  const date = new Date(timestamp)
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+}
+
+export { getFormatedDate }
