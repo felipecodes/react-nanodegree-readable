c30dbab2cdfb827c0f53eb0a2963ccccd1f57a86
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-11-30 22:50:43 -0200

    add: missed file

---
+++ src/components/CommentItem/IconWrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const IconWrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+  padding-top: 4px;
+  padding-right: 8px;
+  padding-bottom: 4px;
+  padding-left: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eee;
+  }
+`
+
+export default IconWrapper
