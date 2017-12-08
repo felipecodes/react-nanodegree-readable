4b021a005243236d1aa596d586779b8ac0909de8
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-12-07 23:19:09 -0200

    add: layout of admin page

--- src/containers/AdminPage/View.js
+++ src/containers/AdminPage/View.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { TextField } from 'material-ui'
+import { RaisedButton } from 'material-ui'
+import { Grid } from '../../components/Grid'
+import { Form } from './Styles'
+
+const rootStyle = { width: '100%' }
+const inputStyle = { fontSize: '14px' }
 
 const View = props => (
-  <div>
-    <form onSubmit={props.handleSubmit}>
-      <input
+  <Grid>
+    <Form onSubmit={props.handleSubmit}>
+      <TextField
+        multiLine
+        style={rootStyle}
+        inputStyle={inputStyle}
         name={'title'}
         value={props.values.title}
+        errorText={props.errors.title}
+        danger={!!props.errors.title}
+        floatingLabelText={'Type your post title'}
         onChange={props.handleChange}
       />
-      <textarea
+
+      <TextField
+        multiLine
+        style={rootStyle}
+        inputStyle={inputStyle}
         name={'body'}
         value={props.values.body}
+        errorText={props.errors.body}
+        danger={!!props.errors.body}
+        floatingLabelText={'Type your post body'}
         onChange={props.handleChange}
       />
-      <button>submit</button>
-    </form>
-  </div>
+
+      <RaisedButton
+        primary
+        buttonStyle={{color: '#fff'}}
+        style={{marginTop: '16px'}}
+        type={'submit'}
+      >
+        submit
+      </RaisedButton>
+    </Form>
+  </Grid>
 )
 
 View.propTypes = {
